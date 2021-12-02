4384163f6c2572eb9a51e3588abbf34d760d42a6
Zuguang Gu <jokergoo@gmail.com>
2021-12-02 12:15:43 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,11 @@
   	msg = paste0("========================================
 ", pkgname, " version ", version, "
 Github page: https://github.com/jokergoo/spiralize
+CRAN page: https://CRAN.R-project.org/package=spiralize
+
+If you use it in published research, please cite:
+Gu, Z. spiralize: an R package for Visualizing Data on Spirals. 
+  Bioinformatics 2021.
 
 This message can be suppressed by:
   suppressPackageStartupMessages(library(spiralize))
